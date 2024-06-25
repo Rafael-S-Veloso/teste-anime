dd31125ce8cc89b8d3a95f3e78da9209c1aaca5c
rafael santos <rafaelleonagraziele@gmail.com>
2024-06-25 16:53:22 -0300

    inicio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,48 @@
-// import { Container } from './styles';
+/* eslint-disable @next/next/no-img-element */
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "../styles/Home.module.css";
 
-function pages() {
+function Pages() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await axios.get(
+          "https://kitsu.io/api/edge/anime?page%5Blimit%5D=5&sort=-average_rating"
+        );
+        const animeData = response.data.data;
+
+        const imageUrls = animeData.map(
+          (anime) => anime.attributes.posterImage.small
+        );
+        setImages(imageUrls);
+      } catch (error) {
+        console.error("Erro ao buscar os dados da API", error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
   return (
-    <div>
-      OLAAAA pages
-      <p>222</p>
+    <div className={styles.container}>
+      <div className={styles.input}>
+        <input type="text" placeholder="Buscar..." className={styles.input} />
+      </div>
+      <div className={styles.imagesContainer}>
+        {images.map((url, index) => (
+          <img
+            key={index}
+            src={url}
+            alt={`Anime ${index}`}
+            className={styles.image}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default pages;
+export default Pages;
